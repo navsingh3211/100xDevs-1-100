24fc4f79e62ce61716e2ec4b9041d300fd6f7e34
navsingh3211 <navsingh3211@gmail.com>
2024-04-08 11:50:24 +0530

    backend in next js

---
+++ next-backend-app/app/page.tsx
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+async function getUserDetails() {
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details");
+	return response.data;
+}
+
+export default async function Home() {
+  const userData = await getUserDetails();
+  console.log(userData,'userData');
+  
+  return (
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
+    </div>
+  );
+}
